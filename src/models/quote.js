a5f6b29b522ea9f8ae52f3dc690b0d5502fb9925
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-25 15:51:57 +0530

    fix(models): update `Datatypes.Array` to ` Datatypes.Array(Datatypes.String)` in tags attribute of model quote.js

--- src/models/quote.js
+++ src/models/quote.js
@@ -1,7 +1,7 @@
-'use strict';
 const {
-  Model
+  Model,
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Quote extends Model {
     /**
@@ -12,13 +12,13 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
+  }
   Quote.init({
     quoteId: DataTypes.STRING,
     content: DataTypes.STRING,
     author: DataTypes.STRING,
     length: DataTypes.INTEGER,
-    tags: DataTypes.ARRAY
+    tags: DataTypes.ARRAY(DataTypes.STRING),
   }, {
     sequelize,
     modelName: 'Quote',
